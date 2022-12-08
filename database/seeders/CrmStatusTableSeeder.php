420043f56507a8379c8c17d265f908e97ceff9c6
Andres Wong <83311143+andyeswong@users.noreply.github.com>
2022-12-08 14:09:15 -0800

    Se agregaron datos al seeder de lead status

--- database/seeders/CrmStatusTableSeeder.php
+++ database/seeders/CrmStatusTableSeeder.php
@@ -28,6 +28,85 @@ class CrmStatusTableSeeder extends Seeder
                 'created_at' => '2021-04-26 22:37:11',
                 'updated_at' => '2021-04-26 22:37:11',
             ],
+            [
+                'id' => 4,
+                'name' => 'New Lead',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Not Interested',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 6,
+                'name' => "Doesn't Qualify",
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 7,
+                'name' => 'Stips Requested',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 8,
+                'name' => 'Stips Received',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 9,
+                'name' => 'Contract Out',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 10,
+                'name' => 'Contract In',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 11,
+                'name' => 'File Submitted',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 12,
+                'name' => 'Canceled',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 13,
+                'name' => 'After File Submitted ad',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 14,
+                'name' => 'ERC Approved',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 15,
+                'name' => 'Client Paid',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+            [
+                'id' => 16,
+                'name' => 'ERC Advance',
+                'created_at' => '2021-04-26 22:37:11',
+                'updated_at' => '2021-04-26 22:37:11',
+            ],
+
         ];
 
         CrmStatus::insert($crmStatuses);
