815321844a15569d3996292a7ba81e09152139b7
Gerardo Osuna <gosuna@farmacias.local>
2023-03-14 10:48:41 -0700

    Cambios en el file uploader

--- app/Console/Commands/leads_uploader.php
+++ app/Console/Commands/leads_uploader.php
@@ -44,6 +44,7 @@ class leads_uploader extends Command
         $user = $this->argument('user_id');
         $csv_file = Reader::createFromPath($csv_path);
         $csv_file->setHeaderOffset(0);
+//        dd($csv_file->getHeader());
         $csv_data = $csv_file->getRecords();
         $csv_file->includeEmptyRecords();
         $csv_file->isEmptyRecordsIncluded();
@@ -53,28 +54,28 @@ class leads_uploader extends Command
         $bar->start();
         $status = CrmStatus::where('name', 'New Lead')->first();
         foreach($csv_data as $row){
-            if(!CrmCustomer::where('phone', '=', $row['PrimaryPhone'])->exists()){
+            if(!CrmCustomer::where('phone', '=', $row['Phone'])->exists()){
                 $crmCustomer_model->create([
-                    'first_name' => $row['FirstName'],
-                    'last_name' => $row['LastName'],
-//                    'email' => $row['Email'],
-                    'phone' => $row['PrimaryPhone'],
+                    'first_name' => $row['First name'],
+                    'last_name' => $row['Last name'],
+                    'email' => $row['Email'],
+                    'phone' => $row['Phone'],
                     'address' => $row['Address'],
                     'city' => $row['City'],
                     'state' => $row['State'],
-                    'zip' => $row['ZipCode'],
-                    'company_name' => $row['BorrowerName'],
-                    'erc_amount' => $row['CurrentApprovalAmount'],
-//                    'w2_employees' => $row['W2_employees'],
-//                    'receive_erc' => $row['Receive_ERC'],
-//                    'ppp_loan' => $row['ppp_loan'],
-//                    'employee_count' => $row['Employee_count'],
-//                    'first_name_verified' => $row['fname_verify'],
-//                    'last_name_verified' => $row['Lname_verify'],
+                    'zip' => $row['Zip'],
+                    'company_name' => $row['company_name'],
+                    'erc_amount' => $row['erc_amount'],
+                    'w2_employees' => $row['W2_employees'],
+                    'receive_erc' => $row['Receive_ERC'],
+                    'ppp_loan' => $row['ppp_loan'],
+                    'employee_count' => $row['Employee_count'],
+                    'first_name_verified' => $row['fname_verify'],
+                    'last_name_verified' => $row['Lname_verify'],
                     'status_id' => $status->id,
                 ]);
             }else{
-                $this->info('Row: '.$bar->getProgress().' - Phone: '.$row['PrimaryPhone'].' already exists');
+                $this->info('Row: '.$bar->getProgress().' - Phone: '.$row['Phone'].' already exists');
             }
             $bar->advance();
         }
